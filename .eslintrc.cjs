6ba45b0c92fe9f887843a946b04d218e5eba3795
Sitdikov-Denis <denis.sitdikov95@yandex.ru>
2022-11-22 10:47:11 +0300

    add greeting, eslint, readline-sync

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb-base'],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 0,
+    'import/extensions': 0,
+  },
+};
